da3f5b53f9f7e0f3d274c63662ed2f3a55aea4ed
Jojo2472 <jolene_fletcher@yahoo.com>
2025-07-07 09:48:18 -0700

    Ensure AlbumClient.tsx is tracked for build

--- app/dashboard/albums/[albumId]/page.tsx
+++ app/dashboard/albums/[albumId]/page.tsx
@@ -1,7 +1,15 @@
 import dynamic from 'next/dynamic';
 
-const AlbumClient = dynamic(() => import('./AlbumClient'), { ssr: false });
+const AlbumClient = dynamic(() => import('./AlbumClient'), {
+  ssr: false,
+  loading: () => <div>Loading album...</div>, // <- this helps prevent the build error
+});
 
 export default function AlbumPage() {
-  return <AlbumClient />;
+  return (
+    <div>
+      <AlbumClient />
+    </div>
+  );
 }
+
