bdf10ae77bb0e9f42ac0ab4eb33eafb867cacf8f
Jojo2472 <jolene_fletcher@yahoo.com>
2025-07-07 17:48:09 -0700

    Fix: Update Album type cover_url to be string | null

--- types.ts
+++ types.ts
@@ -2,8 +2,9 @@ export type Album = {
   id: string;
   name: string;
   description: string | null;
-  cover_url?: string;
+  cover_url: string | null;
   created_at?: string;
   user_id?: string;
-}
+};
+
 
